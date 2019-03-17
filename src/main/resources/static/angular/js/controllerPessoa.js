83aede2f181d2b0c260aa1b35d3bc6973e3b5394
gsvimieiro01 <gsvimieiro@gmail.com>
2019-03-17 17:31:27 -0300

    Ajustes

--- src/main/resources/static/angular/js/controllerPessoa.js
+++ src/main/resources/static/angular/js/controllerPessoa.js
@@ -1,17 +1,18 @@
 angular.module("PessoaApp", [])
 	.value("urlBase", "http://localhost:8080/rest/")
-	.controller("pessoaController", function($http, urlBase){
-	
+	.controller("pessoaController", function($http, urlBase){	
 	
 	var self = this;
 	self.projeto = "Cadastro pessoas com Angular 1";
 	
 	self.pessoa = undefined;
 	self.pessoas = [];
+	self.busca = undefined;
 	
 	self.novo = function(){
 		// cria um objeto vazio
 		self.pessoa = {};
+		self.busca = undefined;
 	};
 	
 	self.salvar = function(){
@@ -22,8 +23,7 @@ angular.module("PessoaApp", [])
 		if (self.pessoa.id){
 			metodo = "PUT";
 			rest = "update";
-		};
-		
+		};		
 		$http({
 			method: metodo,
 			url: urlBase + rest,
@@ -36,6 +36,7 @@ angular.module("PessoaApp", [])
 	};
 	
 	self.alterar = function(dadosPessoa){
+		self.busca = undefined;
 		self.pessoa = dadosPessoa;
 	};
 	
@@ -47,12 +48,25 @@ angular.module("PessoaApp", [])
 			url: urlBase + "remove/" + self.pessoa.id,
 			data: self.pessoa
 		}).then(function successCallback(response){
-			self.atualizarTela();
+			self.atualizarTela();			
 		}, function errorCallback(response){
 			self.ocorreuErro();
 		});
 	};
 	
+	
+	self.pesquisar = function(dadosPessoa){
+		self.pes = dadosPessoa;
+		
+		$http({
+			method:"GET",
+			url: urlBase + "pessoa/" + self.pes.id
+		}).then(function successCallback(response){
+			self.busca = response.data;
+		}, function errorCallback(response){
+			self.ocorreuErro();
+		});
+	};
 	self.ocorreuErro = function(){
 		alert("Erro encontrado ao salvar o registro");
 	};
@@ -64,6 +78,7 @@ angular.module("PessoaApp", [])
 		}).then(function successCallback(response){
 			self.pessoas = response.data;
 			self.pessoa = undefined;
+			self.busca = undefined;
 		}, function erroCallback(response){
 			self.ocorreuErro();
 		});
@@ -73,6 +88,11 @@ angular.module("PessoaApp", [])
 	self.iniciar = function(){
 		self.atualizarTela();
 	};
+	
+	self.voltar = function(){
+		self.busca = undefined;
+		self.atualizarTela();
+	}
 	
 	self.iniciar();
 });
